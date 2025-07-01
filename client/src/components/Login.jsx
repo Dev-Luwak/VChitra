d4446f087aec5d9f0cb6d2e81ebc16caff17aa15
Dev-Luwak <pranavc230@gmail.com>
2025-07-01 10:27:58 +0530

    Initial Commiy: Clean History

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
+import { motion } from "framer-motion"
+
+const Login = () => {
+
+    const [state, setState] = useState('Login')
+    const {setShowLogin} = useContext(AppContext)
+
+    useEffect(()=>{
+        document.body.style.overflow = 'hidden';
+
+        return()=>{
+            document.body.style.overflow = 'unset';
+        }
+    },[])
+
+  return (
+    <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+
+        <motion.form initial={{opacity:0.2, y:50}} transition={{duration: 0.3}} whileInView={{opacity:1,y: 0}} viewport={{once: true}} className='relative bg-white p-10 rounded-xl text-slate-500'>
+            <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
+{state === 'Login' &&
+            <p className='text-sm text-center'>Welcome back! Please sign in to continue</p>
+}
+
+{state !== 'Login' &&
+            <p className='text-sm text-center'>Turn your imagination into visuals</p>
+}
+
+{state !== 'Login' && 
+            <div className='border px-6 py-1 flex items-center gap-2 rounded-full mt-4'>
+                <img src={assets.user_icon} width={30} height={30} alt="" />
+                <input type="text" placeholder='Full Name'  required/>
+            </div>
+}
+
+            <div className='border px-6 py-1 flex items-center gap-2 rounded-full mt-4'>
+                <img src={assets.email_icon}  alt="" />
+                <input type="email" placeholder='Email ID'  required/>
+            </div>
+
+            <div className='border px-6 py-1 flex items-center gap-2 rounded-full mt-4'>
+                <img src={assets.lock_icon}  alt="" />
+                <input type="password" placeholder='Password'  required/>
+            </div>
+
+            <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
+            <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state === 'Login' ? 'Login' : 'Create Account'}</button>
+{state === 'Login' ?
+            <p className='mt-5 text-center'>Don't have an account? <span onClick={()=>setState('Sign Up')} className='text-blue-600 cursor-pointer'>Sign Up</span></p>
+            :
+            <p className='mt-5 text-center'>Already have an account?<span onClick={()=>setState('Login')} className='text-blue-600 cursor-pointer'>Login</span></p>
+}
+
+            <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer' />
+        </motion.form>
+      
+    </div>
+  )
+}
+
+export default Login
