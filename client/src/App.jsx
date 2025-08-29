cb06684cb387b003dfa635ed578fff91fc7d8906
Dev-Luwak <pranavc230@gmail.com>
2025-08-29 21:55:16 +0530

    Deployment error resolution

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 
-import Home from './pages/home'
+import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
